415ac05130498e549608cbcb95630e7ae6cee954
lyzy <1428796682@qq.com>
2020-12-11 23:33:42 +0800

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,10 @@ from app import app
 from app.UserManagement import UserManagement
 from app.TestModule import TestModule
 from app.NLP import NLP
-
+from app.Content import Content
 
 app.register_blueprint(UserManagement,url_prefix='/user')
 app.register_blueprint(TestModule,url_prefix='/test')
 app.register_blueprint(NLP,url_prefix='/nlp')
-
-
+app.register_blueprint(Content, url_prefix='/content')
 
