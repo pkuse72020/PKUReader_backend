554e61c1afb1ff214aef613c20eb4affe5f3158e
gyq <guoyuqi020@126.com>
2020-12-15 22:26:59 +0800

    test(report/) : app report

--- test_module.py
+++ test_module.py
@@ -1,9 +1,9 @@
-# import HtmlTestRunner
-from unittest import defaultTestLoader,TextTestRunner
+from HtmlTestRunner import HTMLTestRunner as TextTestRunner
+from unittest import defaultTestLoader
+# from unittest import TextTestRunner
 
 start_dir = "module-test/"
 discover = defaultTestLoader.discover(start_dir = start_dir, pattern = r'*_test.py')
 if __name__ == "__main__":
-    with open("report.html",'w+') as f:
-        runner = TextTestRunner(stream=f, verbosity = 2)
-        runner.run(discover)
+    runner = TextTestRunner(report_name = "PKUReader_backend_Test_Report", combine_reports = True)
+    runner.run(discover)
