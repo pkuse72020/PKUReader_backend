0bb2c60d6332390a1935e480a75301b9dd928b96
gyq <test@example.com>
2020-12-29 11:10:00 +0800

    hard code admin account

--- app/UserManagement/__init__.py
+++ app/UserManagement/__init__.py
@@ -2,4 +2,17 @@ from flask import Blueprint
 
 UserManagement = Blueprint("UserManagement", __name__)
 
+
+
 from app.UserManagement import routes
+from app.tables import UserInfo
+from app import db
+
+
+info = UserInfo("admin", "admin",True)
+try:
+    db.session.add(info)
+    db.session.commit()
+except Exception as e:
+    db.session.rollback()
+    print(e.args[0])
