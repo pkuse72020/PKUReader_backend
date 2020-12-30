3d3214e76422cc8cc671baadc334e20a468b565c
guoyuqi020 <guoyuqi020@126.com>
2020-12-30 23:04:49 +0800

    fix: test_module.py

--- test_module.py
+++ test_module.py
@@ -2,7 +2,7 @@ from HtmlTestRunner import HTMLTestRunner as TextTestRunner
 from unittest import defaultTestLoader
 # from unittest import TextTestRunner
 
-start_dir = "module-test/"
+start_dir = "moduletest/"
 discover = defaultTestLoader.discover(start_dir = start_dir, pattern = r'*_test.py')
 if __name__ == "__main__":
     runner = TextTestRunner(report_name = "PKUReader_backend_Test_Report", combine_reports = True)
